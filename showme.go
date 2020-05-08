cb0ca8790b8c252e4c0bf220a28823dbba87b574
isalevine <isa.loves.coding@gmail.com>
2020-05-07 20:39:51 -0700

    now formatting title with %20 for urls

--- showme.go
+++ showme.go
@@ -12,7 +12,10 @@ func main() {
 
 	flag.Parse()
 
-	title := flag.Arg(0)
+	// TODO: replace with regex?
+	var title = strings.Replace(flag.Arg(0), " ", "%20", -1)
+	title = strings.Replace(title, "_", "%20", -1)
+
 	url := strings.Join([]string{apiBaseURL, titleQueryURL, title}, "")
 	fmt.Println("url:", url)
 }
